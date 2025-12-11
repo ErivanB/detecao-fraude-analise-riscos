58c26bef0b6e17f86ac0a0e51972cbdff1c48869
Erivan Barros <ErivanB53@gmail.com>
2025-12-10 22:16:32 -0300

    Initial commit

---
+++ src/main/java/com/fraudedetection/detecaofraudeanaliseriscos/DetecaoFraudeAnaliseRiscosApplication.java
@@ -0,0 +1,13 @@
+package com.fraudedetection.detecaofraudeanaliseriscos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetecaoFraudeAnaliseRiscosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetecaoFraudeAnaliseRiscosApplication.class, args);
+    }
+
+}
---
+++ src/main/java/config/FalkorDBConfig.java
@@ -0,0 +1,43 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import com.redislabs.redisgraph.RedisGraphContext;
+
+@Configuration
+public class FalkorDBConfig {
+
+    @Value("${spring.falkordb.host:localhost}")
+    private String host;
+
+    @Value("${spring.falkordb.port:6379}")
+    private int port;
+
+    @Value("${spring.falkordb.password:}")
+    private String password;
+
+    @Value("${spring.falkordb.graph.name:fraude}")
+    private String graphName;
+
+    @Bean
+    public JedisPool jedisPool() {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(10);
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMinIdle(1);
+
+        if (password != null && !password.isEmpty()) {
+            return new JedisPool(poolConfig, host, port, 2000, password);
+        } else {
+            return new JedisPool(poolConfig, host, port, 2000);
+        }
+    }
+
+    @Bean
+    public RedisGraphContext redisGraphContext(JedisPool jedisPool) {
+        return new RedisGraphContext(jedisPool, graphName);
+    }
+}
