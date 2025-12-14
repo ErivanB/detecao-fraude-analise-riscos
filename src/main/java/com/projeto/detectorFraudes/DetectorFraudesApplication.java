29dd64e32a36ec6541098dcf584e540d7dfe77e0
Erick <erickgeovane2002@gmail.com>
2025-12-13 21:17:07 -0300

    Projeto detectorFraudes

--- src/main/java/com/projeto/detectorFraudes/DetectorFraudesApplication.java
+++ src/main/java/com/projeto/detectorFraudes/DetectorFraudesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DetectorFraudesApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(DetectorFraudesApplication.class, args);
 	}
